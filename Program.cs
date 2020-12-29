7b7917b7b7e8ad71f6c449a6f2cb477860d526df
Roman Zayats <70900339+LimS123@users.noreply.github.com>
2020-12-29 19:02:40 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using static LabRab12.Reflector;
+
+namespace LabRab12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetAssembly(typeof(String));
+            Console.WriteLine("\n--------------Вывод конструкторов--------------------------------------------------------\n");
+            var Constructorslist = GetConstructors(typeof(String));
+            foreach (var value in Constructorslist)
+            {
+                Console.WriteLine(value);
+            }
+            Console.WriteLine("\n--------------Вывод public методов--------------------------------------------------------\n");
+            var Methodslist = GetMethods(typeof(String));
+            foreach (var method in Methodslist)
+            {
+                Console.WriteLine(method);
+            }
+            Console.WriteLine("\n--------------Вывод свойств--------------------------------------------------------\n");
+            var Propertieslist = GetProperties(typeof(String));
+            foreach (var property in Propertieslist)
+            {
+                Console.WriteLine(property);
+            }
+            Console.WriteLine("\n-------------Вывод Интерфейсов---------------------------------------------------------\n");
+            var interfacelist = GetInterfaces(typeof(String));
+            foreach (var face in interfacelist)
+            {
+                Console.WriteLine(face);
+            }
+            Console.WriteLine("\n-------------Вывод названий методов---------------------------------------------------------\n");
+            GetMethodNames(typeof(String)); 
+            Console.WriteLine("\n----------------------------------------------------------------------\n");
+        }
+    }
+}
---
+++ Reflector.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+
+namespace LabRab12
+{
+    public static class Reflector
+    {
+        public static void GetAssembly(Type className)
+        {
+            Console.WriteLine(className.Assembly);
+        }
+        public static ConstructorInfo[] GetConstructors(Type className)
+        {
+            return className.GetConstructors();
+        }
+        public static MethodInfo[] GetMethods(Type className)
+        {
+            return className.GetMethods();
+        }
+        public static PropertyInfo[] GetProperties(Type className)
+        {
+            return className.GetProperties();
+        }
+        public static Type[] GetInterfaces(Type className)
+        {
+            return className.GetInterfaces();
+        }
+        public static void GetMethodNames(Type className)
+        {
+            foreach (var method in className.GetMethods())
+            {
+
+                Console.WriteLine(method.Name);
+            }
+        }
+    }
+}
